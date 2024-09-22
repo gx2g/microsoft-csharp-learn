53f99eb78d27dbd5edf268a546eadd5a95d760ac
Robert R <grafx2g@gmail.com>
2024-09-21 22:18:27 -0700

    changes to app

--- app/Program.cs
+++ app/Program.cs
@@ -7,3 +7,8 @@ Console.WriteLine('b');
 Console.WriteLine(123);
 // floating point literal
 Console.WriteLine(3.14159);
+
+Console.WriteLine(34.40M);
+
+
+Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
